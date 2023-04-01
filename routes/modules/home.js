b5a121fc3859024d75e84e28b81c24d9ce1fe9b8
Ray7808 <yuwang7808@gmail.com>
2023-04-01 14:57:29 +0000

    feat: add handlebars and setting template engine

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    res.send("This is just test")
+    res.render("index")
 })
 
 module.exports = router
