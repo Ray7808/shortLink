e01b8e51f03187dd1cbbe71e4b3cc69bd5bc299d
Ray7808 <yuwang7808@gmail.com>
2023-04-03 05:07:48 +0000

    feat: add copy function to copy the short URL (+1 squashed commit) Squashed commits: [054a551] feat: add copy function to copy the short URL (+1 squashed commit) Squashed commits: [74b7183] feat: add copy function to copy the short URL

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,56 +1,52 @@
-const express = require("express")
-const router = express.Router()
-const shortLinkPair = require("../../models/shortLinkPair")
-const shortLink = require("../../shortLink")
-const initialURL = "http://localhost:3000/" // 之後要修改網址可以只改這行
+const express = require('express');
+const router = express.Router();
+const shortLinkPair = require('../../models/shortLinkPair');
+const shortLink = require('../../shortLink');
+const initialURL = 'http://localhost:3000/' + 'redirect/'; // 之後要修改網址可以只改這行前半
 
-router.use(express.static("public"))
-
-router.get("/", (req, res) => {
-    res.render("index")
-})
-router.get("/:shortWords", (req, res) => {
+router.get('/', (req, res) => {
+    // 顯示頁面供使用者輸入網址
+    res.render('index');
+});
+router.get('/redirect/:shortWords', (req, res) => {
     //透過回傳的短網址，從mongoDB找到對應的網址並重新導向
-    const shortWords = req.params.shortWords
+    const shortWords = req.params.shortWords;
     shortLinkPair
         .find({ outputLink: shortWords })
         .lean()
-        .then((inputLink) => {
-            res.redirect(inputLink[0].inputLink)
+        .then((LinkPair) => {
+            res.redirect(LinkPair[0].inputLink);
         })
-        .catch((error) => console.log(error))
-})
-router.get("/success/:shortWords", (req, res) => {
+        .catch((error) => console.log(error));
+});
+router.get('/success/:shortWords', (req, res) => {
     // 顯示成功的短網址連結
-    const shortWords = req.params.shortWords
-    res.render("success", { initialURL: initialURL, shortWords: shortWords })
-})
-router.post("/shortLink", (req, res) => {
+    const shortWords = req.params.shortWords;
+    res.render('success', { initialURL: initialURL, shortWords: shortWords });
+});
+router.post('/shortLink', (req, res) => {
     // 先確認資料庫有沒有對應的URL，沒有就使用shortLink隨機產生新的shortURL
-    const inputURL = req.body.inputURL
-    let outputURL = ""
-    let shortWords = ""
+    const inputURL = req.body.inputURL;
+    let outputURL = '';
+    let shortWords = '';
 
     shortLinkPair
         .find({ inputLink: inputURL })
         .lean()
         .then((outputLink) => {
-            outputURL = outputLink
-
+            outputURL = outputLink;
             if (outputURL.length) {
-                console.log(`You find URL inside mongoBD`)
-                shortWords = outputURL[0].outputLink
-                res.redirect(`/success/${shortWords}`)
+                shortWords = outputURL[0].outputLink;
+                res.redirect(`/success/${shortWords}`);
             } else {
-                console.log(`You don't find URL inside mongoBD and you should generate a new one`)
-                shortWords = shortLink()
+                shortWords = shortLink();
                 return shortLinkPair
                     .create({ inputLink: inputURL, outputLink: shortWords })
                     .then(() => res.redirect(`/success/${shortWords}`))
-                    .catch((error) => console.log(error))
+                    .catch((error) => console.log(error));
             }
         })
-        .catch((error) => console.log(error))
-})
+        .catch((error) => console.log(error));
+});
 
-module.exports = router
+module.exports = router;
